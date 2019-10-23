9f3d29f4ba406c854b4d18f4028a64c3f7a109bd
ciakarellas <you@example.com>
2019-10-23 16:39:26 +0200

    add StreamBuilder to Dashboard widget for adding new bill in SliverList

--- lib/widgets/dashboard.dart
+++ lib/widgets/dashboard.dart
@@ -1,60 +1,65 @@
 import 'package:budget_app/database/data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatelessWidget {
-  final database = AppDatabase();
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top:200),
-          child: Text(
-            "gds",
-            style: TextStyle(fontSize: 30, fontFamily: 'Aleo Light'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top:100),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 180,
-                child: TextFormField(
-                  //controller: _sumController,
-                  style: TextStyle(),
-                  keyboardType: TextInputType.datetime,
-                  decoration: InputDecoration()
-                ),
+    final database = Provider.of<AppDatabase>(context);
+    return StreamBuilder(
+      stream: database.watchAllBills(),
+      builder: (context, snapshot) {
+        return Center(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top:200),
+              child: Text(
+                "gds",
+                style: TextStyle(fontSize: 30, fontFamily: 'Aleo Light'),
               ),
-              /*DropdownButton(
-                items: sum.category.map<DropdownMenuItem>((String newCategory){
-                  return DropdownMenuItem<String>(
-                    value: newCategory,
-                    child: Text(newCategory),
-                  );
-                }).toList(),
-                onChanged: (category){
-                  sum.selectedCategory = category;
-                },
-                value: sum.selectedCategory,
-              ),*/
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    child: TextFormField(
+                      //controller: _sumController,
+                      style: TextStyle(),
+                      keyboardType: TextInputType.datetime,
+                      decoration: InputDecoration()
+                    ),
+                  ),
+                  /*DropdownButton(
+                    items: sum.category.map<DropdownMenuItem>((String newCategory){
+                      return DropdownMenuItem<String>(
+                        value: newCategory,
+                        child: Text(newCategory),
+                      );
+                    }).toList(),
+                    onChanged: (category){
+                      sum.selectedCategory = category;
+                    },
+                    value: sum.selectedCategory,
+                  ),*/
+                ],
+              ),
+            ),
+            FlatButton(
+              child: Icon(Icons.add),
+              onPressed: (){
+                final newBill = new Bill(comment: "test");
+                database.insertNewBill(newBill);
+              },
+            ),
+          ],
         ),
-        FlatButton(
-          child: Icon(Icons.add),
-          onPressed: (){
-            final newBill = new Bill(comment: "test");
-            database.insertNewBill(newBill);
-          },
-        ),
-      ],
-    ),
+        );
+      }
     );;
   }
   
